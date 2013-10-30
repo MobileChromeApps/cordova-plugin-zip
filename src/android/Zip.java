5835d15c78fdc55e2da8f25666eeb52c1d23b2d4
remi-parain <rparain@ubidreams.com>
2013-10-30 17:39:26 +0100

    add missing import for FileNotFoundException on Android

--- src/android/Zip.java
+++ src/android/Zip.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
