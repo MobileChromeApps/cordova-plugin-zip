96bee7883554ac778e8394ff60f798afffe21760
Ian Clelland <iclelland@chromium.org>
2014-02-24 15:46:12 -0500

    Resolve unzip() URL arguments to file locations (fixes: #15)

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ A Cordova plugin to unzip files in Android and iOS.
 
 ##Usage
 
-        zip.unzip('full file path', 'directory to extract into', function(){
-            console.log('All done');
-        });
+    zip.unzip(<source zip>, <destination dir>, <callback>);
+
+Both source and destination arguments can be URLs obtained from the HTML File
+interface or absolute paths to files on the device.
+
+The callback argument will be executed when the unzip is complete, or when an
+error occurs. It will be called with a single argument, which will be 0 on
+success, or -1 on failure.
--- src/ios/Zip.m
+++ src/ios/Zip.m
@@ -1,6 +1,24 @@
 #import "Zip.h"
+#import "CDVFile.h"
 
 @implementation Zip
+
+- (NSString *)pathForURL:(NSString *)urlString
+{
+    // Attempt to use the File plugin to resolve the destination argument to a
+    // file path.
+    NSString *path;
+    id filePlugin = [self.commandDelegate getCommandInstance:@"File"];
+    if (filePlugin != nil) {
+        CDVFilesystemURL* url = [CDVFilesystemURL fileSystemURLWithString:urlString];
+        path = [filePlugin filesystemPathForURL:url];
+    }
+    // If that didn't work for any reason, return the original argument.
+    if (path == nil) {
+        path = urlString;
+    }
+    return path;
+}
 
 - (void)unzip:(CDVInvokedUrlCommand*)command
 {
@@ -8,9 +26,13 @@
         CDVPluginResult* pluginResult = nil;
         
         @try {
-            NSString *zipPath = [command.arguments objectAtIndex:0] ;
-            NSString *destinationPath = [command.arguments objectAtIndex:1];
+            NSString *zipURL = [command.arguments objectAtIndex:0];
+            NSString *destinationURL = [command.arguments objectAtIndex:1];
             NSError *error;
+
+            NSString *zipPath = [self pathForURL:zipURL];
+            NSString *destinationPath = [self pathForURL:destinationURL];
+
             if([SSZipArchive unzipFileAtPath:zipPath toDestination:destinationPath overwrite:YES password:nil error:&error]) {
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
             } else {
