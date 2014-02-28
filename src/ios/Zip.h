3e30945b0deb10880d31f708b0ef926edef3d25c
Andreas Dangel <adangel@users.sourceforge.net>
2014-02-28 20:11:03 +0100

    Add progress feedback while unzipping (iOS).

--- src/ios/Zip.h
+++ src/ios/Zip.h
@@ -2,8 +2,13 @@
 #import <Cordova/CDVPlugin.h>
 #import "SSZipArchive.h"
 
-@interface Zip : CDVPlugin
+@interface Zip : CDVPlugin <SSZipArchiveDelegate> {
+    @private
+    CDVInvokedUrlCommand* _command;
+}
+
 
 - (void)unzip:(CDVInvokedUrlCommand*)command;
+- (void)zipArchiveProgressEvent:(NSInteger)loaded total:(NSInteger)total;
 
 @end
