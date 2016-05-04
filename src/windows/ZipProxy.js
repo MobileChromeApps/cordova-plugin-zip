9572bffc6c89fe46f9437f1d75311b4b1e656fb1
dwolner <daniel.wolner@gmail.com>
2016-05-04 12:07:38 -0700

    updates to keepCallback

--- src/windows/ZipProxy.js
+++ src/windows/ZipProxy.js
@@ -1,26 +1,25 @@
 cordova.commandProxy.add("Zip", {
         unzip:function(successCallback,errorCallback,args) {
-            
+         
+            var file = args[0];
+            var dir = args[1];
 
-        var file = args[0];
+            var plugin = ZipCSComponent.ZipPlugin();
 
-        var dir = args[1];
-
-        var plugin = ZipCSComponent.ZipPlugin();
-
-        plugin.unZipAsync(file, dir, function (percent) {
-            successCallback(new ProgressEvent("OK", { "loaded": percent, "total": 100 }));
-            // todo: fix
-        })
-        .done(
-            function () {
-                successCallback();
-            },
-            function (error) {
-                errorCallback(error);
-            }
-        );
-
+            plugin.unZipByUriAsync(file, dir, function (loaded, total) {
+            //plugin.unZipByPathAsync(file, dir, function (loaded, total) {
+                var progressEvent = new ProgressEvent("OK", { loaded: loaded, total: total })
+                var callbackParams = { keepCallback: true };
+                successCallback(progressEvent, callbackParams);
+            })
+            .done(
+                function () {
+                    successCallback();
+                },
+                function (error) {
+                    errorCallback(error);
+                }
+            );
 
         }
     });
